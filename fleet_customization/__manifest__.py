d3222d7b99f1f9391bc5333f19fe0167bea155c5
annykiran2704 <kirankantesariya@gmail.com>
2022-11-06 22:45:36 +0530

    Add changes in the module

---
+++ fleet_customization/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Fleet Customization",
+    'version': "15.0.0.0",
+    'category': "Fleet",
+    'summary': "Fleet Customization.",
+    'description': """Fleet Customization.""",
+    'author': "",
+    "website": "",
+    "price": 00.00,
+    'currency': "EUR",
+    'depends': ['base', 'sale', 'fleet', 'sale_renting', 'hr', 'car_repair_industry'],
+    'data': [
+        'views/rental_orders_views.xml',
+    ],
+    'license':'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    "live_test_url": '',
+    "images":[""],
+}
